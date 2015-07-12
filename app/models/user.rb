3ed38799c5f314610ecd035546d0dfaccc63603f
y-matsumoto <mycb750@gmail.com>
2015-07-12 23:02:49 +0900

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  
+  has_and_belongs_to_many :roles
+
+  def has_role?(name)
+    self.roles.where(name: name).length > 0
+  end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
